51225a41ae1e5235b324edb7a527bb47c5d816b1
Snapple49 <oliver-dot-stein@hotmail.com>
2018-02-08 12:08:40 +0100

    bugfix, fix urlencode

--- harmonicIO/master/rest_service.py
+++ harmonicIO/master/rest_service.py
@@ -316,7 +316,7 @@ class ClientManager(object):
 
             # send request to worker
             worker_url = "http://{}:8081/docker?token=None&command=create".format(candidates[0][0])
-            with urlopen(worker_url, job_data) as response:
+            with urlopen(worker_url, urlencode(job_data)) as response:
                 html = response.read()
 
             worker_response = html.decode('UTF-8')

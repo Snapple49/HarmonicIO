ef8ce1e56891abc97d8d094b54b93b9966eb294a
Snapple49 <oliver-dot-stein@hotmail.com>
2018-07-04 17:13:13 +0200

    all changes for last commit didnt come along

--- harmonicIO/master/__main__.py
+++ harmonicIO/master/__main__.py
@@ -20,7 +20,7 @@ def run_irm():
     """
     if Setting.get_autoscaling():    
         IntelligentResourceManager.start_irm(BinPacking.first_fit)
-        if IntelligentResourceManager.__container_manager:
+        if IntelligentResourceManager.container_manager:
             SysOut.out_string("Autoscaling supervisor started")
         else:
             SysOut.terminate_string("Error: could not start IRM service")
